d827080b047a16fee3bc9f3cde8821826e721407
Chris McIntosh <chris_mcintosh@bridge360.com>
2016-12-12 21:59:22 -0600

    adding makelinks

---
+++ makelinks.sh
@@ -0,0 +1,8 @@
+mv ~/.config/i3/config ~/.config/i3/config.bak
+ln -s ~/.config/i3/config ~/dotfiles/.i3config 
+ln -s ~/dotfiles/.i3config ~/.config/i3/config
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.vim ~/.vim
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/.bashrc ~/.bashrc
+ln -s ~/dotfiles/.compton.conf ~/.config/compton/compton.conf
