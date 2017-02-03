e4a42070b70d594885f981b3cb7ab740893f2b36
Chris McIntosh <rougechampion2002@gmail.com>
2017-02-03 09:35:27 -0600

    adding install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Pre-Reqs
+# Need git installed to pull this script
+# Ansible and this script should handle the rest
+
+apt-get install ansible
+
+ansible-playbook user_env.yaml
+
+cp .bashrc /home/$USER/.bashrc
+cp -r .vim /home/$USER/.vim
+cp .vimrc /home/$USER/.vimrc
+cp .tmux.conf /home/$USER/.vimrc
