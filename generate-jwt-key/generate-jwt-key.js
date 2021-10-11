a8f98a1f6839232125cb194a6f103bc1e75090f9
Chris McIntosh <chocrates@github.com>
2021-10-10 21:21:48 -0500

    Previously we were hardcoding the app id which obviously doesn't work

--- generate-jwt-key/generate-jwt-key.js
+++ generate-jwt-key/generate-jwt-key.js
@@ -6,9 +6,15 @@ const main = async () => {
         description: "Path to the private key used to generate the jwt",
         global: true,
         demandOption: true,
+    })
+    .option("app-id", {
+        alias: "i",
+        description: "Application Id",
+        global: true,
+        demandOption: true
     }).argv;
     var privateKey = fs.readFileSync(argv.privateKey, "utf8");
-    const auth = createAppAuth({ appId: 115727, privateKey: privateKey });
+    const auth = createAppAuth({ appId: argv.appId, privateKey: privateKey });
     const jwt = await auth({ type: "app" });
     console.log(jwt.token);
 };
